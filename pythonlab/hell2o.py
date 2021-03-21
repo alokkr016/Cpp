0e473d97452f8a60f0443a572318dc458d340f16
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-03-21 21:19:54 +0530

    Renaming

--- pythonlab/hell2o.py
+++
@@ -1,7 +0,0 @@
-def printnumber(n):
-    i = 0
-    while i < n:
-        print(i+1)
-        i += 1
-n = int(input("Enter your number of your choice"))
-printnumber(n)
--- pythonlab/reversefirstandlastletter.py
+++ pythonlab/reversefirstandlastletter.py
@@ -2,4 +2,3 @@ str = "Chandigarh"
 n = len(str)
 newStr = str[n-1] + str[1:n-1] + str[0]
 print(newStr)
-n = 'Chand igarh'[::-1]
