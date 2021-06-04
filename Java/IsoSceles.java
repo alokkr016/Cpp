a5e8690244d10af3e2852f90abef439878ab5bf7
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-06-04 11:13:47 +0530

    Pyramid program

--- Java/IsoSceles.java
+++ Java/IsoSceles.java
@@ -1,3 +1,33 @@
-public class IsoSceles{
+import java.util.Scanner;
 
+public class IsoSceles{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while (i <= n){
+            int space = 1;
+            while(space <= n - i){
+                System.out.print(" ");
+                space++;
+            }
+            int j = 1;
+            int p = 1;
+            while (j <= i){
+                System.out.print(p);
+                j++;
+                p++;
+            }
+            int k = 1;
+            p = i - 1;
+            while (k <= i - 1){
+                System.out.print(p);
+                k++;
+                p++;
+            }
+            System.out.println();
+            i++;
+
+        }
+    }
 }
